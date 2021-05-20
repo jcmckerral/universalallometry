bfb92b3bf05d42dbd76f9c1b5160b792b9ddc7a7
jcmckerral <46704491+jcmckerral@users.noreply.github.com>
2021-05-20 16:09:38 +0930

    Add files via upload

---
+++ generatefigures/assignBaseParameters_attackform.m
@@ -0,0 +1,14 @@
+%%
+
+r0 = 3E-9; 
+d0 = 1E-8;
+b0 =  1;
+h0 = 100;  
+K0 = 0.05;
+
+sigmar = -1/4;
+sigmad = -1/4;
+sigmab = 0.5;
+sigmahR = 1/2;
+sigmahC = -1/4;
+sigmaK = -3/4;
--- generatefigures/loggedODE.m
+++ generatefigures/loggedODE.m
@@ -1,1 +1,8 @@
+function dydt = loggedODE(t,X,mu,gamma,omega)
+% log the ODE to help numerical solver over the large size range
 
+% Rosenzweig-Macarthur equations
+dydt =   [(1-(exp(X(1))/mu)) - (gamma*exp(X(2)))/(1+exp(X(1)));
+        (gamma*exp(X(1)))/(1+exp(X(1))) - omega];
+
+end
